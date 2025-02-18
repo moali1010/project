c78a08edfa202402d93e77148c93bd902bc9a8b5
moali1010 <moali.develop@gmail.com>
2025-02-18 11:07:10 +0330

    Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    # Implement the expected sections here
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_filter = ['gender', 'is_staff', 'is_superuser', 'is_active']
+    sortable_by = ['username']
+    list_editable = ['is_staff', 'is_active']
+    fieldsets = (
+        (None, {
+            'fields': (
+                'username', 'password'
+            )
+        }
+         ),
+        ('Personal info', {
+            'fields': (
+                'first_name', 'last_name', 'email', 'gender', 'age', 'description'
+            )
+        }
+         ),
+        ('Contact info', {
+            'fields': (
+                'phone', 'address'
+            )
+        }
+         ),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'
+            )
+        }
+         ),
+        ('Important dates', {
+            'fields': (
+                'last_login', 'date_joined'
+            )
+        }
+         )
+    )
