bac41b3a536c193c0268d89db1837b0d7ec25018
moali1010 <moali.develop@gmail.com>
2025-05-10 15:10:07 +0330

    Done Task

--- charities/views.py
+++ charities/views.py
@@ -119,4 +119,15 @@ class TaskResponse(APIView):
 
 
 class DoneTask(APIView):
-    pass
+    permission_classes = (IsCharityOwner,)
+
+    def post(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        if task.state != 'A':
+            data = {'detail': 'Task is not assigned yet.'}
+            return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+        else:
+            task.state = 'D'
+            task.save()
+            data = {'detail': 'Task has been done successfully.'}
+            return Response(data=data, status=status.HTTP_200_OK)
